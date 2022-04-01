4f899d9358ef4e55ad62cb46f69aaa04d5a4725c
Jason <J.Jacoby15@gmail.com>
2022-04-01 00:39:45 -0700

    Added dynamic grid size using UI slider input, added hide/show grid functionality, added a display of current color

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
 const grid = document.querySelector('#grid');
 let size = 24
-let penColor = '#209D88'
+let penColor = '#4A0D67'
+let colorDisplay = document.querySelector('#currentColor')
 
 function gridLayout(size){
     grid.style.gridTemplateRows = `repeat(${size}, 1fr)`
     grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`
 }
 
+makeGrid()
 
-
-for(let i = 0; i < size * size; i++){
-    let square = document.createElement('div')
-    square.addEventListener('mouseenter', color)
-    square.classList.add('square')
-    grid.appendChild(square)
+function makeGrid(){
+    for(let i = 0; i < size * size; i++){
+        let square = document.createElement('div')
+        square.addEventListener('mouseenter', color)
+        square.classList.add('square')
+        grid.appendChild(square)
+    }
 }
 
 function color(e){
@@ -21,10 +24,10 @@ function color(e){
 }
 
 const reset = document.querySelector('#reset')
-const allSquares = document.querySelectorAll('.square')
-reset.addEventListener('click', test)
+let allSquares = document.querySelectorAll('.square')
+reset.addEventListener('click', resetSquares)
 
-function test(e){
+function resetSquares(e){
     for(let i = 0; i < allSquares.length; i++ ){
         allSquares[i].style.backgroundColor = 'transparent';
     }
@@ -35,8 +38,7 @@ colorPicker.addEventListener('input', pickColor)
 
 function pickColor(){
     penColor = colorPicker.value;
-    console.log(penColor)
-    return penColor
+    colorDisplay.style.backgroundColor = penColor
 }
 
 const eraser = document.querySelector('#eraser')
@@ -44,15 +46,42 @@ eraser.addEventListener('click', erase)
 
 function erase(){
     penColor = 'transparent'
-    return penColor
+    colorDisplay.style.backgroundColor = 'white'
 }
 
 let gridSize = document.querySelector('#gridSize')
 gridSize.addEventListener('change', setSize)
 
 function setSize(){
-    size = gridSize.value
-    return size
+    removeGrid(grid);
+    size = gridSize.value;
+    gridLayout(size);
+    makeGrid()
+    allSquares = document.querySelectorAll('.square')
 }
 
 gridLayout(size)
+
+function removeGrid(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
+
+function hideGrid(e){
+    for(let i = 0; i < allSquares.length; i++ ){
+        allSquares[i].style.outline = 'transparent';
+    }
+}
+
+function showGrid(e){
+    for(let i = 0; i < allSquares.length; i++ ){
+        allSquares[i].style.outline = 'rgba(11, 112, 243, 0.3) solid 1px';
+    }
+}
+
+const hide = document.querySelector('#hide');
+hide.addEventListener('click', hideGrid)
+
+const show = document.querySelector('#show')
+show.addEventListener('click', showGrid)
